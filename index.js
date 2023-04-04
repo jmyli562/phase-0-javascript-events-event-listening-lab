c6116bcc713aa80313fc1d2ab6a4b4d4d655de84
Jimmy Li <jmyli562@gmail.com>
2023-04-04 10:22:46 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
 }
+
+function clickAlert() {
+    return alert('I was clicked hahah!');
+  }
+
+  function removeIdMain(){
+    const removeMain = document.getElementById('main');
+    removeMain.addEventListener('click', function(e){
+        e.target.remove();
+    })
+  }
+
+  removeIdMain()
+  addingEventListener();
